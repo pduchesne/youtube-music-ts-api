6ed8b59a793222ffb58692d93c6fa00413b4482c
pduchesne <pduchesne@gmail.com>
2021-12-23 16:33:06 +0100

    add history fetcher and parser

--- src/parsers/base-parser.ts
+++ src/parsers/base-parser.ts
@@ -2,16 +2,22 @@ export default class BaseParser {
     traverse(obj: any, ...path: string[]): any {
         if (obj) {
             if (Array.isArray(path) && path.length > 0)  {
+                const subPath = path.slice(1);
                 if (Array.isArray(obj) && path[0] === "*") {
+                    const result = [];
                     for (let i = 0; i < obj.length; i++) {
-                        const subObj = this.traverse(obj[i], ...path.slice(1));
+                        const subObj = this.traverse(obj[i], ...subPath);
                         if (subObj) {
-                            return subObj;
+                            if (Array.isArray(subObj)) {
+                                result.push(...subObj);
+                            } else {
+                                result.push(subObj);
+                            }
                         }
                     }
-                    return undefined;
+                    return result;
                 } else {
-                    return this.traverse(obj[path[0]], ...path.slice(1));
+                    return this.traverse(obj[path[0]], ...subPath);
                 }
             }
             return obj;
